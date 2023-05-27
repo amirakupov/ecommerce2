8711eb328c29592ef6c6b0ece566dd762cfde690
amirakupov <amirakupov@gmail.com>
2023-05-27 14:59:54 +0200

    cart update for unlogged user

--- static/js/cart.js
+++ static/js/cart.js
@@ -14,7 +14,24 @@ for (i = 0; i < updateBtns.length; i++) {
 	})
 }
 function addCookeiItem(productId, action){
-    console.log('User is not authenticated')
+    console.log('bro is not authenticated')
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+			cart[productId] = {'quantity':1}
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+		if (cart[productId]['quantity'] <= 0) {
+			console.log('Item should be deleted')
+			delete cart[productId]
+		}
+	}
+	console.log('Cart: ', cart)
+	document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	location.reload()
 }
 function updateUserOrder(productId, action){
     console.log('ye, bro is logged in')
